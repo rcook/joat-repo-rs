f25c1eeaab7b2918ffb8e2c3f9edfcfa471f59df
Richard Cook <rcook@rcook.org>
2023-05-13 06:19:45 -0700

    Improve list command output

--- src/list_command.rs
+++ src/list_command.rs
@@ -4,29 +4,33 @@ use anyhow::Result;
 use colored::Colorize;
 
 pub fn do_list(repo: &Repo) -> Result<Status> {
-    println!("{}", "Metadirectories".green());
     let mut manifests = repo.list_manifests()?;
     manifests.sort_unstable_by_key(|m| m.manifest.meta_id);
     let manifests = manifests;
-    for m in manifests {
-        println!(
-            "  {} ({})",
-            m.manifest.meta_id.as_simple().to_string().yellow(),
-            m.data_dir.display().to_string().blue()
-        );
+    if !manifests.is_empty() {
+        println!("{}", "Metadirectories".green());
+        for m in manifests {
+            println!(
+                "  {} ({})",
+                m.manifest.meta_id.as_simple().to_string().yellow(),
+                m.data_dir.display().to_string().blue()
+            );
+        }
     }
 
-    println!("{}", "Links".green());
     let mut links = repo.list_links()?;
     links.sort_unstable_by_key(|l| l.link.project_dir.clone());
     let links = links;
-    for l in links {
-        println!(
-            "  {} ({}) ({})",
-            l.link.link_id.as_str().yellow(),
-            l.link.project_dir.display().to_string().blue(),
-            l.link.meta_id.as_simple().to_string().yellow()
-        );
+    if !links.is_empty() {
+        println!("{}", "Links".green());
+        for l in links {
+            println!(
+                "  {} ({}) ({})",
+                l.link.link_id.as_str().yellow(),
+                l.link.project_dir.display().to_string().blue(),
+                l.link.meta_id.as_simple().to_string().yellow()
+            );
+        }
     }
 
     Ok(Status::Success)
